6dcc2520270275c080634a093a53996a78aa6e9e
Arthur aa. Alastuey <aalastuey@racine.2isa.fr>
2018-04-27 11:39:20 +0200

    v0 apprentissage laravel

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,16 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', ['as' => 'home', function(){
+  return 'Je suis la page d\'accueil !';
+}]);
+
+Route::get('{n}', function($n) {
+    return Response::make('Je suis la page ' . $n . ' !', 200);
+})->where('n', '[1-3]');
+
+Route::get('article/{n}', function($n) {
+    return view('article', ['numero' => $n]);
+    return view('article')->withNumero($n);
+    return view('article')->with('numero', $n);
+})->where('n', '[0-9]+');
