de2ed0b023def44dd2d102fa9af7a232de9e7857
Arthur aa. Alastuey <aalastuey@racine.2isa.fr>
2018-04-27 16:23:06 +0200

    a

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,8 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', ['as' => 'home', function(){
-  return 'Je suis la page d\'accueil !';
+  return view('welcome');
 }]);
 
-Route::get('{n}', function($n) {
-    return Response::make('Je suis la page ' . $n . ' !', 200);
-})->where('n', '[1-3]');
-
-Route::get('article/{n}', function($n) {
-    return view('article', ['numero' => $n]);
-    return view('article')->withNumero($n);
-    return view('article')->with('numero', $n);
-})->where('n', '[0-9]+');
+Route::get('contact',[])
+Route::post('contact',['uses'=>'GestionController'])
