5598f44e537b62ae040d21f5284f5e3b09e0929c
Arthur aa. Alastuey <aalastuey@racine.2isa.fr>
2018-06-13 10:09:55 +0200

    Préparation au déploiement

--- resources/views/mail.blade.php
+++ resources/views/mail.blade.php
@@ -1,9 +1,9 @@
 <h3>{{ $interlocuteur->civilite}} {{ $interlocuteur->prenom }} {{ $interlocuteur->nom }},</h3>
 
-<p>Je vous informe que des informations vous relatant ont été ajoutés à la base de données de l'association AMIO en vue d'une gestion des partenariats.
+<p>Je vous informe que des informations vous relatant ont été ajoutées à la base de données de l'association AMIO en vue d'une gestion des partenariats.
 Ce mail vous est envoyé dans le cadre du respect de vos données selon la loi RGPD.</p>
 
-<p>Si vous voulez que vos informations soit retirées de la base envoyez un mail à ce sujet à cette adresse : arthur.alastuey@2isa.org.</p>
+<p>Si vous voulez que vos informations soit retirées de la base envoyez un mail à ce sujet à cette adresse : [Ajouter votre adresse mail ici].</p>
 
 <h4>Application de gestion des partenariats<h5>
 <img height="50%" widht="50%" src="http://www.amio-millau.fr/static/images/amio/AMIO-logo.png"/>
